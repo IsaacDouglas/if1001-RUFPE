696a1e8e77495764ac976ab7b44d96f2d426226d
Isaac Douglas <isaacdouglas2015@gmail.com>
2019-05-28 22:21:18 -0300

    fragment favoritos

--- RUFPE/app/src/main/java/br/ufpe/cin/rufpe/FavoritesFragment.kt
+++ RUFPE/app/src/main/java/br/ufpe/cin/rufpe/FavoritesFragment.kt
@@ -1,11 +1,20 @@
 package br.ufpe.cin.rufpe
 
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_favorites.*
+import kotlinx.android.synthetic.main.item_favorites.view.*
 
 class FavoritesFragment : Fragment() {
 
@@ -14,8 +23,57 @@ class FavoritesFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_favorites, container, false)
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        favorites_recycler_view.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = ItemAdapter(context, favorites)
+            addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
+        }
+    }
+
     companion object Factory {
         fun newInstance(): FavoritesFragment = FavoritesFragment()
+        var favorites = arrayOf("ONE", "TWO")
+    }
+
+    internal inner class ItemAdapter (
+        var c: Context,
+        var items: Array<String>) :  RecyclerView.Adapter<ItemAdapter.ItemHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+            val view = LayoutInflater.from(c).inflate(R.layout.item_favorites, parent, false)
+            return ItemHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+            val item = items[position]
+            holder.name.text = item
+        }
+
+        override fun getItemCount(): Int {
+            return items.size
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        internal inner class ItemHolder(val item: View) : RecyclerView.ViewHolder(item) {
+            val name: TextView = item.item_name
+            private val button: Button = item.item_button
+
+            init {
+                item.setOnClickListener { _ ->
+                    Toast.makeText(c, name.text, Toast.LENGTH_LONG)
+                }
+
+                button.setOnClickListener { _ ->
+                    Toast.makeText(c, name.text, Toast.LENGTH_LONG)
+                }
+            }
+        }
     }
 
 }
